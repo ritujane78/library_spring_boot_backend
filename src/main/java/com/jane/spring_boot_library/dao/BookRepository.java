0526d23f7b0ac8e3b5e24ac7825d853630a4ca2e
Ritu Bafna <ritujane78@gmail.com>
2025-10-11 12:30:16 +0545

    make search books workable

--- src/main/java/com/jane/spring_boot_library/dao/BookRepository.java
+++ src/main/java/com/jane/spring_boot_library/dao/BookRepository.java
@@ -1,9 +1,16 @@
 package com.jane.spring_boot_library.dao;
 
 import com.jane.spring_boot_library.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title , Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
