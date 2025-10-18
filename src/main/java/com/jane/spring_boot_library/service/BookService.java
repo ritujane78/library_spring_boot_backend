3558d8a727990364350d3069f2be554981d4d449
Ritu Bafna <ritujane78@gmail.com>
2025-10-18 08:50:19 +0545

    implement history section in shelf

--- src/main/java/com/jane/spring_boot_library/service/BookService.java
+++ src/main/java/com/jane/spring_boot_library/service/BookService.java
@@ -2,8 +2,10 @@ package com.jane.spring_boot_library.service;
 
 import com.jane.spring_boot_library.dao.BookRepository;
 import com.jane.spring_boot_library.dao.CheckoutRepository;
+import com.jane.spring_boot_library.dao.HistoryRepository;
 import com.jane.spring_boot_library.entity.Book;
 import com.jane.spring_boot_library.entity.Checkout;
+import com.jane.spring_boot_library.entity.History;
 import com.jane.spring_boot_library.responsemodels.ShelfCurrentLoansResponse;
 
 import org.springframework.stereotype.Service;
@@ -24,9 +26,12 @@ public class BookService {
 
     private CheckoutRepository checkoutRepository;
 
-    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+    private HistoryRepository historyRepository;
+
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository, HistoryRepository historyRepository) {
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
+        this.historyRepository = historyRepository;
     }
 
     public Book checkoutBook(String userEmail, Long bookId) throws Exception{
@@ -119,6 +124,18 @@ public class BookService {
 
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
+
+        History history = new History(
+            userEmail,
+                validateCheckout.getCheckoutDate(),
+                LocalDate.now().toString(),
+                book.get().getTitle(),
+                book.get().getAuthor(),
+                book.get().getDescription(),
+                book.get().getImg()
+        );
+
+        historyRepository.save(history);
     }
     public void renewLoan(String userEmail, Long bookId) throws Exception {
 
